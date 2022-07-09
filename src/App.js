07b9b8a582a4972305a6866d119ab4dd0a962cf7
Khai Truong <kminht@gmail.com>
2022-07-09 12:55:44 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Dogs from "./components/pages/Dogs";
+import About from "./components/pages/About";
+import SignUp from "./components/pages/SignUp";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Dogs');
+
+  const renderPage = () => {
+    if (currentPage === 'Dogs') {
+      return <Dogs/>
+    }
+    if (currentPage === 'About') {
+      return <About/>
+    }
+    if (currentPage === 'SignUp') {
+      return <SignUp/>
+    }
+  }
+
+  useEffect(() => {
+    document.title = currentPage;
+  })
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar onPageChange={handlePageChange}/>
+      {renderPage()}
     </div>
-  );
+  )
 }
 
 export default App;
