40c7b247477df2b0aeb2630b28032bf2dc8f0ac1
Khai Truong <kminht@gmail.com>
2022-07-09 13:30:33 -0400

    src and components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import React, { useState, useEffect } from "react";
-import Dogs from "./components/pages/Dogs";
-import About from "./components/pages/About";
-import SignUp from "./components/pages/SignUp";
-import NavBar from "./components/NavBar";
+// import Dogs from "./components/pages/Dogs";
+// import About from "./components/pages/About";
+// import SignUp from "./components/pages/SignUp";
+// import NavBar from "./components/NavBar";
+import Display from "./components/Display";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Dogs');
+  // const [currentPage, setCurrentPage] = useState('Dogs');
 
-  const renderPage = () => {
-    if (currentPage === 'Dogs') {
-      return <Dogs/>
-    }
-    if (currentPage === 'About') {
-      return <About/>
-    }
-    if (currentPage === 'SignUp') {
-      return <SignUp/>
-    }
-  }
+  // const renderPage = () => {
+  //   if (currentPage === 'Dogs') {
+  //     return <Dogs/>
+  //   }
+  //   if (currentPage === 'About') {
+  //     return <About/>
+  //   }
+  //   if (currentPage === 'SignUp') {
+  //     return <SignUp/>
+  //   }
+  // }
 
-  useEffect(() => {
-    document.title = currentPage;
-  })
+  // useEffect(() => {
+  //   document.title = currentPage;
+  // })
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  }
+  // const handlePageChange = (page) => {
+  //   setCurrentPage(page);
+  // }
 
   return (
-    <div>
-      <NavBar onPageChange={handlePageChange}/>
-      {renderPage()}
+    // <div>
+    //   <NavBar onPageChange={handlePageChange}/>
+    //   {renderPage()}
+    // </div>
+    <div> 
+        hello 
+      <br></br>
+      <Display/>
     </div>
   )
 }
---
+++ src/components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Display() {
+    return(
+        <div>
+        hello display
+        </div>
+    )
+}
